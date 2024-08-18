e43b488488790b9b4d3006134638ba41741e4944
nvstefan <nvstefan@mail.ru>
2024-08-18 10:28:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-number_of_completed_tasks = 12
-number_of_hours_spent = 1.5
-time_task = number_of_hours_spent / number_of_completed_tasks
-course_name = "Python,"
-print(course_name ,'всего задач: ' + str(number_of_completed_tasks) + ', затрачено часов: '+ str(number_of_hours_spent) + ', среднее время выполнения' ,time_task ,"часа.")
+name = 'Name: Николай'
+print(name)
+age = 68
+print('Age:',age)
+age = age +1
+print('New Age:',age)
+is_studen = True
+print('Is Student:',is_studen)
